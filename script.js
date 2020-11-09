b15f0185f592a641b97ddec4f8f4728cc128f3f0
codeologie <manafsultan90@gmail.com>
2020-11-10 00:51:17 +0100

    add the 3 musketeers html css js

---
+++ script.js
@@ -0,0 +1,73 @@
+const quizData = [
+  {
+    question: 'What is the capital of Chile?',
+    a: 'Santiago',
+    b: '',
+    c: 'None of the answers',
+    d: '',
+    correct: 'a'
+  }, {
+    question: 'In bowling, what is the term given for three consecutive strikes?',
+    a: 'None of the answers',
+    b: '',
+    c: 'A turkey',
+    d: '',
+    correct: 'c'
+  }, {
+    question: 'Who was the only British Prime Minister to be assassinated?',
+    a: '',
+    b: 'Spencer Perceval',
+    c: '',
+    d: 'None of the answers',
+    correct: 'b'
+  }, {
+    question: 'Which country is the origin of the cocktail Mojito?',
+    a: '',
+    b: 'Cuba',
+    c: '',
+    d: 'None of the answers',
+    correct: 'b'
+  }, {
+    question: 'Which vitamin is the only one that you will not find in an egg?',
+    a: 'None of the answers',
+    b: 'Vitamin B',
+    c: 'Vitamin C',
+    d: '',
+    correct: 'c'
+  }, {
+    question: 'What was Britney Spears’ first single called?',
+    a: 'Baby One More Time',
+    b: '',
+    c: 'None of the answers',
+    d: '',
+    correct: 'a'
+  }, {
+    question: 'Which singer was known amongst other things as The King of Pop?',
+    a: '',
+    b: 'None of the answers',
+    c: '',
+    d: ' Michael Jackson',
+    correct: 'd'
+  }, {
+    question: 'Taylor Swift grew up on what type of farm?',
+    a: 'None of the answers',
+    b: '',
+    c: 'Christmas Tree Farm',
+    d: '',
+    correct: 'c'
+  }, {
+    question: 'Ataulfo, Alphonso and Keitt are varieties of what fruit?',
+    a: 'Mango',
+    b: '',
+    c: '',
+    d: 'None of the answers',
+    correct: 'a'
+  }, {
+    question: 'Who invented jeans?',
+    a: '',
+    b: 'None of the answers',
+    c: '',
+    d: 'Levi Strauss',
+    correct: 'd'
+  },
+];
